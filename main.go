0a5f9d7d4a7ebf96c4409449b04eac823c816cdc
daranivar <arenitas_5000@hotmail.com>
2024-09-02 16:50:32 -0300

    primer commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	//Crea un router con Gin
+	router := gin.Default()
+
+	//Captura la solicitud GET "/hello-world"
+	router.GET("/hello-world", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	router.Run() // Corremos nuestro servidor sobre el puerto 8080
+
+}
